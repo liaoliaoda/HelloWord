46799de9cbe54cedac7b3f58b1a0509575c74ed4
liaofuda <327107646@qq.com>
2018-04-02 16:37:31 +0800

    logs

---
+++ OAUS.Core/IOausService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Innov.Core
+{
+    public interface IInnovService
+    {
+        int GetLatestVersion();
+    }
+}
---
+++ OAUS.Server/OausService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Innov.Core;
+
+namespace Innov.Server
+{
+    internal class InnovService : MarshalByRefObject, IInnovService
+    {
+        private UpdateConfiguration fileConfig;
+        public InnovService(UpdateConfiguration _fileConfig)
+        {
+            this.fileConfig = _fileConfig;
+        }
+
+        public override object InitializeLifetimeService()
+        {
+            return null;
+        }
+
+        public int GetLatestVersion()
+        {
+            return this.fileConfig.ClientVersion;
+        }
+    }
+}
